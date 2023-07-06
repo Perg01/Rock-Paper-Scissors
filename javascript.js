776d3007ebd93242d759da39024d873798f5c9dc
Perg01 <jsatshs@gmail.com>
2023-07-05 21:21:16 -0700

    Finished the requirements, will be back to add html and css

--- javascript.js
+++ javascript.js
@@ -8,21 +8,63 @@ function getComputerChoice() {
     }
 }
 // console.log(getComputerChoice());
-function playRound(playerSelection, computerSelection) {
-    if (!(playerSelection === "rock" || "paper" || "scissor")) {
-        return "Invalid answer! Your options are either 'rock', 'paper' or 'scissor'";
-    }
-    while (playerSelection === "rock") {
-        if (computerSelection === "Rock") {
-            return "Draw!";
-        } else if (computerSelection === "Paper") {
-            return "You lose! Paper beats rock.";
-        } else if (computerSelection === "Scissor") {
-            return "You win! Rock beats Scissor.";
-        }
-    }
+function game() {
+    let botWins = 0;
+    let playerWins = 0;
+    let matchesDraw = 0;
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Rock, Paper or Scissor?").toLowerCase();
 
+        function playRound(playerSelection, computerSelection) {
+            if (!(playerSelection === "rock" || "paper" || "scissor")) {
+                return "Invalid answer! Your options are either 'rock', 'paper' or 'scissor'";
+            }
+            while (playerSelection === "rock") {
+                if (computerSelection === "Rock") {
+                    matchesDraw++;
+                    return "Draw!";
+                } else if (computerSelection === "Paper") {
+                    botWins++;
+                    return "You lose! Paper beats Rock.";
+                } else if (computerSelection === "Scissor") {
+                    playerWins++;
+                    return "You win! Rock beats Scissor.";
+                }
+            }
+            while (playerSelection === "paper") {
+                if (computerSelection === "Paper") {
+                    matchesDraw++;
+                    return "Draw!";
+                } else if (computerSelection === "Scissor") {
+                    botWins++;
+                    return "You lose! Scissor beats Paper.";
+                } else if (computerSelection === "Rock") {
+                    playerWins++;
+                    return "You win! Paper beats Rock.";
+                }
+            }
+            while (playerSelection === "scissor") {
+                if (computerSelection === "Scissor") {
+                    matchesDraw++;
+                    return "Draw!";
+                } else if (computerSelection === "Paper") {
+                    playerWins++;
+                    return "You win! Scissor beats Paper.";
+                } else if (computerSelection === "Rock") {
+                    botWins++;
+                    return "You lose! Rock beats Scissor.";
+                }
+            }
+        }
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${playerWins}, Bot: ${botWins}, Draws: ${matchesDraw}`);
+    }
+    while (playerWins == botWins) {
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Rock, Paper or Scissor?").toLowerCase();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player: ${playerWins}, Bot: ${botWins}, Draws: ${matchesDraw}`);
+    }
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
